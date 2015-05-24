5fd67446cb727bd448a6c7c86fa2ce1fd3131572
Daniel Imms <tyriar@tyriar.com>
2015-05-24 10:07:29 -0700

    Fix counting sort breakage

--- test/counting-sort-spec.js
+++ test/counting-sort-spec.js
@@ -13,15 +13,14 @@ describe("counting-sort", function () {
 
         if (minValue >= 0) {
           it(test.it, function () {
-            expect(algorithm(original, maxValue))
-              .toEqual(sorted);
+            expect(algorithm(original, maxValue)).toEqual(sorted);
           });
         }
       })(testHelper.integerTests[i]);
     }
   });
 
-  // Test sort(array, minValue, maxValue) for arrays with a specific range.
+  // Test sort(array, maxValue, minValue) for arrays with a specific range.
   describe("sort(array, maxValue, minValue)", function () {
     for (var i = 0; i < testHelper.integerTests.length; i++) {
       (function (test) {
@@ -31,24 +30,9 @@ describe("counting-sort", function () {
         var maxValue = sorted[sorted.length - 1];
 
         it(test.it, function () {
-          expect(algorithm(original, maxValue, minValue))
-            .toEqual(sorted);
+          expect(algorithm(original, maxValue, minValue)).toEqual(sorted);
         });
       })(testHelper.integerTests[i]);
     }
-  });
-
-  describe("sort(...)", function () {
-    it("should throw an exception with 1 argument", function () {
-      expect(function () {
-        algorithm(1);
-      }).toThrow("Cannot sort with counting sort with 1 arguments");
-    });
-
-    it("should throw an exception with > 3 arguments", function () {
-      expect(function () {
-        algorithm(1, 2, 3, 4);
-      }).toThrow("Cannot sort with counting sort with 4 arguments");
-    });
   });
 });
